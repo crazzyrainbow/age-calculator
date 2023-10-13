17028b93df21d088df00c406e33ab84481b84891
crazzyrainbow <ankitkumarsuman6@gmail.com>
2023-10-13 22:22:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import './App.css'; // You may have other styles here
+import AgeCalculator from './AgeCalculator'; // Import the AgeCalculator component
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to Age Calculator App</h1>
       </header>
+      <main>
+        <AgeCalculator /> {/* Render the AgeCalculator component */}
+      </main>
     </div>
   );
 }
